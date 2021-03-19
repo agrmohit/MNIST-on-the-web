782aa1769797a1f88b68dd7a174f18ba523ae4ae
Harshit Dixit <harshitdxt3004@gmail.com>
2021-03-20 00:41:56 +0530

    Canvas created

---
+++ canvas/src/Components/Canvas.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CanvasDraw from "react-canvas-draw";
+
+const Canvas = () => {
+    return (
+        <div>
+            <CanvasDraw />
+        </div>
+    )
+}
+
+export default Canvas;
