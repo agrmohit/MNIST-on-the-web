2008bda07d764cfdd5a4d8401e6f07b88a539a51
Harshit Dixit <harshitdxt3004@gmail.com>
2021-03-20 21:40:37 +0530

    Model trained

--- MNIST-server/app.py
+++ MNIST-server/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import torch
+import time
+import numpy as np
+from torchvision.transforms import transforms
+from main import Net
 app = Flask(__name__)
+cors = CORS(app)
+PATH = "../MNIST-model/mnist_cnn.pt"
 
+model = Net().double()
+model.load_state_dict(torch.load(PATH))
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+trans = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    body = request.get_json()
+    print(body)
+    img_array = np.asarray(body['indices'])
+    img_array = trans(img_array)
+    img_array = img_array.view(1,1,28,28)
+    output = torch.exp(model(img_array.double()))
+    return jsonify({"output": int(np.argmax(output.detach().numpy()))})
+
 
 if __name__ == '__main__':
     app.run()
